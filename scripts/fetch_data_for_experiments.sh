4f41493454c8fdad9d734e18d88045639392a82e
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-16 16:08:29 +0100

    Update Google Drive link for the scalabilty experiments

--- scripts/fetch_data_for_experiments.sh
+++ scripts/fetch_data_for_experiments.sh
@@ -2,7 +2,7 @@ cd ..
 cd data/
 cd scalability/
 pip install gdown
-gdown https://drive.google.com/uc?id=13vP-a2axw0CRJynLIdE_9L2uDMN4a2cv
+gdown https://drive.google.com/uc?id=1YBALSVclnFLMCLCnE7I50-tXF335cF1T
 tar -xvf scalability.tgz
 rm scalability.tgz
 mv -v scalability/* ./
