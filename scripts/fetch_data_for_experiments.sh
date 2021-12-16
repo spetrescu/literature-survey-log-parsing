fe3dc61a454a3c4f3e63fd37ed75b591ef7c7a7b
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-16 14:48:40 +0100

    Update workflow for fetching data for scalability experiments

--- scripts/fetch_data_for_experiments.sh
+++ scripts/fetch_data_for_experiments.sh
@@ -1,6 +1,9 @@
 cd ..
 cd data/
+cd scalability/
 pip install gdown
 gdown https://drive.google.com/uc?id=13vP-a2axw0CRJynLIdE_9L2uDMN4a2cv
 tar -xvf scalability.tgz
 rm scalability.tgz
+mv -v scalability/* ./
+rm -rf scalability/
