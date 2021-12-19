aaa03991dbb2528b230fec3103a6b3dbf8bd1ff2
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-19 13:25:44 +0100

    Add code for LogMine experiments

---
+++ experiments/python2/logparser/benchmark/LogMine.sh
@@ -0,0 +1,61 @@
+for d in Andriod HDFS Thunderbird Windows
+do
+  for s in 1
+  do
+    for r in 1
+    do
+        echo "Dataset $d size $s run no $r"
+        python LogMine_benchmark.py $d $s
+    done
+  done
+done
+
+
+for s in 1
+do
+  for r in 1
+  do
+      echo "Dataset OpenSSH size $s run no $r"
+      python LogMine_benchmark.py OpenSSH $s
+  done
+done
+
+for s in 1
+do
+  for r in 1
+  do
+      echo "Dataset BGL size $s run no $r"
+      python LogMine_benchmark.py BGL $s
+  done
+done
+
+#for d in Andriod HDFS Thunderbird Windows
+#do
+#  for s in 1 2 4 10 20 50 100 200 300 500 1000
+#  do
+#    for r in 1 2 3 4 5 6 7 8 9 10
+#    do
+#        echo "Dataset $d size $s run no $r"
+#        python LogMine_benchmark.py $d $s
+#    done
+#  done
+#done
+#
+#
+#for s in 1 2 4 20 50 100 200 300 500
+#do
+#  for r in 1 2 3 4 5 6 7 8 9 10
+#  do
+#      echo "Dataset OpenSSH size $s run no $r"
+#      python LogMine_benchmark.py OpenSSH $s
+#  done
+#done
+#
+#for s in 1 2 4 20 50 100 200 300
+#do
+#  for r in 1 2 3 4 5 6 7 8 9 10
+#  do
+#      echo "Dataset BGL size $s run no $r"
+#      python LogMine_benchmark.py BGL $s
+#  done
+#done
