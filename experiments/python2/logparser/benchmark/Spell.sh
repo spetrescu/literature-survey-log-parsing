6b55f7b63ede42d1d5499be177257c72b5d46ad8
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-19 11:15:46 +0100

    Add code for Spell experiments

---
+++ experiments/python2/logparser/benchmark/Spell.sh
@@ -0,0 +1,55 @@
+for d in Andriod HDFS Thunderbird Windows
+do
+  for s in 1
+  do
+    for r in 1
+    do
+        python Spell_benchmark.py $d $s
+    done
+  done
+done
+
+
+for s in 1
+do
+  for r in 1
+  do
+      python Spell_benchmark.py OpenSSH $s
+  done
+done
+
+for s in 1
+do
+  for r in 1
+  do
+      python Spell_benchmark.py BGL $s
+  done
+done
+
+#for d in Andriod HDFS Thunderbird Windows
+#do
+#  for s in 1 2 4 10 20 50 100 200 300 500 1000
+#  do
+#    for r in 1 2 3 4 5 6 7 8 9 10
+#    do
+#        python Spell_benchmark.py $d $s
+#    done
+#  done
+#done
+#
+#
+#for s in 1 2 4 20 50 100 200 300 500
+#do
+#  for r in 1 2 3 4 5 6 7 8 9 10
+#  do
+#      python Spell_benchmark.py OpenSSH $s
+#  done
+#done
+#
+#for s in 1 2 4 20 50 100 200 300
+#do
+#  for r in 1 2 3 4 5 6 7 8 9 10
+#  do
+#      python Spell_benchmark.py BGL $s
+#  done
+#done
