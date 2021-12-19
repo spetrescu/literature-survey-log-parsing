66936e66bfa2f14004b72ceec6e5bd431eacea64
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-19 13:07:41 +0100

    Update bash script for running SLCT experiments

--- experiments/python2/logparser/benchmark/SLCT.sh
+++ experiments/python2/logparser/benchmark/SLCT.sh
@@ -1,39 +1,8 @@
-for d in Andriod HDFS Thunderbird Windows
-do
-  for s in 1
-  do
-    for r in 1
-    do
-        echo "Dataset $d size $s run no $r"
-        python SLCT_benchmark.py $d $s
-    done
-  done
-done
-
-
-for s in 1
-do
-  for r in 1
-  do
-      echo "Dataset OpenSSH size $s run no $r"
-      python SLCT_benchmark.py OpenSSH $s
-  done
-done
-
-for s in 1
-do
-  for r in 1
-  do
-      echo "Dataset BGL size $s run no $r"
-      python SLCT_benchmark.py BGL $s
-  done
-done
-
 #for d in Andriod HDFS Thunderbird Windows
 #do
-#  for s in 1 2 4 10 20 50 100 200 300 500 1000
+#  for s in 1
 #  do
-#    for r in 1 2 3 4 5 6 7 8 9 10
+#    for r in 1
 #    do
 #        echo "Dataset $d size $s run no $r"
 #        python SLCT_benchmark.py $d $s
@@ -42,20 +11,53 @@ done
 #done
 #
 #
-#for s in 1 2 4 20 50 100 200 300 500
+#for s in 1
 #do
-#  for r in 1 2 3 4 5 6 7 8 9 10
+#  for r in 1
 #  do
 #      echo "Dataset OpenSSH size $s run no $r"
 #      python SLCT_benchmark.py OpenSSH $s
 #  done
 #done
 #
-#for s in 1 2 4 20 50 100 200 300
+#for s in 1
 #do
-#  for r in 1 2 3 4 5 6 7 8 9 10
+#  for r in 1
 #  do
 #      echo "Dataset BGL size $s run no $r"
 #      python SLCT_benchmark.py BGL $s
 #  done
 #done
+
+# Andriod
+
+for d in HDFS Thunderbird Windows
+do
+  for s in 1 2 4 10 20 50 100 200 300 500 1000
+  do
+    for r in 1 2 3 4 5 6 7 8 9 10
+    do
+        echo "Dataset $d size $s run no $r"
+        python SLCT_benchmark.py $d $s
+    done
+  done
+done
+
+
+for s in 1 2 4 20 50 100 200 300 500
+do
+  for r in 1 2 3 4 5 6 7 8 9 10
+  do
+      echo "Dataset OpenSSH size $s run no $r"
+      python SLCT_benchmark.py OpenSSH $s
+  done
+done
+
+for s in 1 2 4 20 50 100 200 300
+do
+  for r in 1 2 3 4 5 6 7 8 9 10
+  do
+      echo "Dataset BGL size $s run no $r"
+      python SLCT_benchmark.py BGL $s
+  done
+done
