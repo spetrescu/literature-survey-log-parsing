2f3d871c55e197965e595e3e47e012ec52e3d3a5
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-19 02:49:30 +0100

    Add fetch data script

--- experiments/python2/logparser/fetch_data.sh
+++ experiments/python2/logparser/fetch_data.sh
@@ -1,3 +1,5 @@
+pip install uni2ascii
+
 pip install gdown
 gdown https://drive.google.com/uc?id=1zVaXpR7lOy3p1VwCLouHMDP6vtx69kLC
 
@@ -5,21 +7,69 @@ tar -xvf scalability.tgz
 rm scalability.tgz
 
 rm scalability/Andriod/Andriod_2k.log
+cd scalability/
+cd Andriod/
+for s in 1 4 20 50 100 200 300 500 1000
+do
+    uni2ascii Andriod_${s}k.log > ${s}k.log && mv ${s}k.log Andriod_${s}k.log
+done
+cd ..
+cd ..
 cp -v scalability/Andriod/* logs/Andriod/
 
 rm scalability/BGL/BGL_2k.log
+cd scalability/
+cd BGL/
+for s in 1 4 20 50 100 200 300
+do
+    uni2ascii BGL_${s}k.log > ${s}k.log && mv ${s}k.log BGL_${s}k.log
+done
+cd ..
+cd ..
 cp -v scalability/BGL/* logs/BGL/
 
 rm scalability/HDFS/HDFS_2k.log
+cd scalability/
+cd HDFS/
+for s in 1 4 20 50 100 200 300 500 1000
+do
+    uni2ascii HDFS_${s}k.log > ${s}k.log && mv ${s}k.log HDFS_${s}k.log
+done
+cd ..
+cd ..
 cp -v scalability/HDFS/* logs/HDFS/
 
 rm scalability/OpenSSH/OpenSSH_2k.log
+cd scalability/
+cd OpenSSH/
+for s in 1 4 20 50 100 200 300 500
+do
+    uni2ascii OpenSSH_${s}k.log > ${s}k.log && mv ${s}k.log OpenSSH_${s}k.log
+done
+cd ..
+cd ..
 cp -v scalability/OpenSSH/* logs/OpenSSH/
 
 rm scalability/Thunderbird/Thunderbird_2k.log
+cd scalability/
+cd Thunderbird/
+for s in 1 4 20 50 100 200 300 500 1000
+do
+    uni2ascii Thunderbird_${s}k.log > ${s}k.log && mv ${s}k.log Thunderbird_${s}k.log
+done
+cd ..
+cd ..
 cp -v scalability/Thunderbird/* logs/Thunderbird/
 
 rm scalability/Windows/Windows_2k.log
+cd scalability/
+cd Windows/
+for s in 1 4 20 50 100 200 300 500 1000
+do
+    uni2ascii Windows_${s}k.log > ${s}k.log && mv ${s}k.log Windows_${s}k.log
+done
+cd ..
+cd ..
 cp -v scalability/Windows/* logs/Windows/
 
 rm -rf scalability/
