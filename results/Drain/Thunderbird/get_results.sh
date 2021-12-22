c8370ed5f67a6bff39325be7a44b1db279fadbee
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-22 16:23:46 +0200

    Add Drain accuracy and scalability results

---
+++ results/Drain/Thunderbird/get_results.sh
@@ -0,0 +1,17 @@
+mkdir res/
+sed '/^Processed/d' raw/raw_results.txt > 1.txt
+sed '/^Used/d' 1.txt > 2.txt
+sed '/^Parsing\ file/d' 2.txt > 3.txt
+sed '/^\ \ \ \ \ \ \ \ \ F/d' 3.txt > 4.txt
+sed '/^===\ O/d' 4.txt > 5.txt
+sed '/^\ \ \ \ \ \ \ \ \ \ \ \ \ F/d' 5.txt > 6.txt
+sed '/^===\ E/d' 6.txt > res/res_results.txt
+
+rm 1.txt
+rm 2.txt
+rm 3.txt
+rm 4.txt
+rm 5.txt
+rm 6.txt
+
+python generate_final_results.py
