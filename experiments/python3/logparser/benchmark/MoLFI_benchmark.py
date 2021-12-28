c6c499345b83262c522256d3541947f649b5721c
Stefan Petrescu <stefan.petrescu997@gmail.com>
2021-12-28 20:34:34 +0100

    Add code for MoLFI experiments

--- experiments/python3/logparser/benchmark/MoLFI_benchmark.py
+++ experiments/python3/logparser/benchmark/MoLFI_benchmark.py
@@ -5,7 +5,11 @@ sys.path.append('../')
 from logparser import MoLFI, evaluator
 import os
 import pandas as pd
+from memory_profiler import memory_usage
 
+n = len(sys.argv)
+DATASET = str(sys.argv[1])
+SIZE = str(sys.argv[2])
 
 input_dir = '../logs/' # The input directory of log file
 output_dir = 'MoLFI_result/' # The output directory of parsing results
@@ -108,24 +112,31 @@ benchmark_settings = {
         }
 }
 
-bechmark_result = []
-for dataset, setting in benchmark_settings.items():
-    print('\n=== Evaluation on %s ==='%dataset)
-    indir = os.path.join(input_dir, os.path.dirname(setting['log_file']))
-    log_file = os.path.basename(setting['log_file'])
-
+def parsing_logs(setting, indir, output_dir, log_file):
     parser = MoLFI.LogParser(log_format=setting['log_format'], indir=indir, outdir=output_dir, rex=setting['regex'])
     parser.parse(log_file)
-    
-    F1_measure, accuracy = evaluator.evaluate(
-                           groundtruth=os.path.join(indir, log_file + '_structured.csv'),
-                           parsedresult=os.path.join(output_dir, log_file + '_structured.csv')
-                           )
-    bechmark_result.append([dataset, F1_measure, accuracy])
 
+bechmark_result = []
+for dataset, setting in benchmark_settings.items():
+    if dataset == DATASET:
+        print('\n=== Evaluation on %s ==='%dataset)
+        logfile  = str(DATASET + "/" + DATASET + "_" + SIZE + "k.log")
+        indir = os.path.join(input_dir, os.path.dirname(logfile))
+        log_file = os.path.basename(logfile)
 
-print('\n=== Overall evaluation results ===')
-df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'F1_measure', 'Accuracy'])
-df_result.set_index('Dataset', inplace=True)
-print(df_result)
-df_result.T.to_csv('MoLFI_bechmark_result.csv')
+        mem = max(memory_usage((parsing_logs, (setting, indir, output_dir, log_file))))
+        print("Used memory")
+        print(mem)
+        if SIZE == "2":
+            F1_measure, accuracy = evaluator.evaluate(
+                groundtruth=os.path.join(indir, log_file + '_structured.csv'),
+                parsedresult=os.path.join(output_dir, log_file + '_structured.csv')
+            )
+            bechmark_result.append([dataset, F1_measure, accuracy])
+
+if SIZE == "2":
+    print('\n=== Overall evaluation results ===')
+    df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'F1_measure', 'Accuracy'])
+    df_result.set_index('Dataset', inplace=True)
+    print(df_result)
+    df_result.T.to_csv('MoLFI_bechmark_result.csv')
