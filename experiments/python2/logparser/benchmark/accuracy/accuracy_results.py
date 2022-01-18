e6828b65e4fb8084e05a5f1644e9108288b75f23
Stefan Petrescu <stefan.petrescu997@gmail.com>
2022-01-18 13:16:46 +0100

    Update workflow Python 2 accuracy experiments

--- experiments/python2/logparser/benchmark/accuracy/accuracy_results.py
+++ experiments/python2/logparser/benchmark/accuracy/accuracy_results.py
@@ -1,13 +1,17 @@
 import csv
 import sys
+import os
 
 n = len(sys.argv)
 METHOD = str(sys.argv[1])
 RESULTS_FILE = f"res/{METHOD}_raw_terminal_logs_results.txt"
 
-DIR_PATH = "final/"
-SAVE_PATH = f"final/accuracy_res_{METHOD}.csv"
+DIR_PATH = "accuracy_results/"
+SAVE_PATH = f"accuracy_results/accuracy_res_{METHOD}.csv"
 method = METHOD
+
+if not os.path.exists(DIR_PATH):
+    os.makedirs(DIR_PATH)
 
 row_list = [["method", "dataset", "log_size", "precision", "recall", "F1_measure", "parsing_accuracy", "iteration_no"]]
 with open(f'{SAVE_PATH}', 'w', newline='') as file:
